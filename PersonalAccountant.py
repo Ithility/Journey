04a21fb46759a516b547879cb8384c1d0f3f7f6e
Vlastimir <149712502+Ithility@users.noreply.github.com>
2024-01-03 18:34:54 +0100

    Update PersonalAccountant.py

--- PersonalAccountant.py
+++ PersonalAccountant.py
@@ -1,11 +1,14 @@
 import pandas as pd
-import numpy as np
 
-
+# Your existing DataFrame
 spending = pd.DataFrame({
-"Categories": ['Survivables', 'Bills', 'Treats', 'Clothing', 'Luxuries'],
-"MonthlySpending $": ['250', '150', '100', '0', '80'],
-"DailySpending $": ['50', '0', '20', '0', '5']
+    "Categories": ['Survivables', 'Bills', 'Treats', 'Clothing', 'Luxuries'],
+    "MonthlySpending $": ['250', '150', '100', '0', '80'],
+    "DailySpending $": ['50', '0', '20', '0', '5']
 })
+
+# Convert spending columns to numeric values
+spending["MonthlySpending $"] = pd.to_numeric(spending["MonthlySpending $"])
+spending["DailySpending $"] = pd.to_numeric(spending["DailySpending $"])
 
 print(spending)
